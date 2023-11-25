df8bec4872f8c896d8c5b473f39946cfa771591d
TricksXTech <78645286+TricksXTech@users.noreply.github.com>
2023-11-25 22:15:54 +0530

    Update cwtip.php

--- CWTip_WHMCS_v.6.x.x/CWTip/gateways/callback/cwtip.php
+++ CWTip_WHMCS_v.6.x.x/CWTip/gateways/callback/cwtip.php
@@ -9,42 +9,49 @@ require_once(dirname(__FILE__) . '/../cwtip-sdk/encdec_cwtip.php');
 $gatewaymodule = "cwtip"; 
 $GATEWAY = getGatewayVariables($gatewaymodule);
 
-$response = array();
-$response = $_POST;   
-  
-if(isset($response['ORDERID']) && isset($response['STATUS']) && isset($response['RESPCODE']) && $response['RESPCODE'] != 325){
+// Die if module is not active.
+if (!$GATEWAY['type']) {
+    die("Module Not Activated");
+}
 
-	$txnid_arr  = explode('_',$response['ORDERID']);
-	$tnxid = $txnid_arr[0];
-	$txnid  = $txnid_arr[0];
-	$status =$response['STATUS'];
-	$paytm_trans_id = $response['TXNID'];
-	$checksum_recv='';	
-	$amount=$response['TXNAMOUNT'];
-	if(isset($response['CHECKSUMHASH'])){
-		$checksum_recv=$response['CHECKSUMHASH'];
-	}
+if(isset($_POST['ORDERID']) && isset($_POST['STATUS']) && isset($_POST['RESPCODE']) && $_POST['RESPCODE'] != 325){
+   
+    $txnid_arr  = explode('_',$_POST['ORDERID']);
+	$tnxid = $txnid_arr[0]; 
+	$txnid  = checkCbInvoiceID($_POST['ORDERID'],'cwtip');	
+	$status = $_POST['STATUS'];
+	$paytm_trans_id = $_POST['TXNID'];
+	$amount = $_POST['TXNAMOUNT'];
 	
-	if($status== 'TXN_SUCCESS'){	
+	$ORDER_ID = $_POST['ORDER_ID'];
+	$acc_id = $_POST['MID'];
+	$auth_token = $_POST['MAT'];
 	
-		if($response['STATUS']=='TXN_SUCCESS')
-		{
-			$gatewayresult = "success";
+	if($status== 'TXN_SUCCESS'){
+
+$send_data = file_get_contents("https://ca.cwtip.co/api/?api=$acc_id&auth=$auth_token&orderid=$ORDER_ID"); 
+$json_data = json_decode($send_data,true);
+
+if($json_data["status"]=="failed"){
+    logTransaction($GATEWAY["name"], $_POST, "It seems some issue in server to server communication. Kindly connect with administrator.");
+}
+else if($json_data["STATUS"]=="TXN_FAILURE" || $json_data["RESPMSG"]=="Invalid Order Id."){
+    logTransaction($GATEWAY["name"], $_POST, "It seems some issue in server to server communication. Kindly connect with administrator.");
+}
+else{
+ 
+	 $gatewayresult = "success";
 			addInvoicePayment($txnid, $paytm_trans_id, $amount,"0.0", $gatewaymodule); 
-			logTransaction($GATEWAY["name"], $response, $response['RESPMSG']);
+			logTransaction($GATEWAY["name"], $_POST, $_POST['RESPMSG']);
 		}
-		else{
-			logTransaction($GATEWAY["name"], $response, "It seems some issue in server to server communication. Kindly connect with administrator.");
-		}
-	} elseif ($status != "TXN_SUCCESS") {
-		logTransaction($GATEWAY["name"], $response, "Checksum Mismatch");
-	}else {
-		logTransaction($GATEWAY["name"], $response, $response['RESPMSG']); 
 	}
-	
+		else{
+			logTransaction($GATEWAY["name"], $_POST, "It seems some issue in server to server communication. Kindly connect with administrator.");
+		}
 	$returnResponse=(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http")."://$_SERVER[HTTP_HOST]$_SERVER[SCRIPT_NAME]";
 	$filename=str_replace('modules/gateways/callback/cwtip.php','viewinvoice.php?id='.$txnid, $returnResponse);
     header("Location: $filename");
+    
 }
 else{
 
@@ -55,4 +62,5 @@ else{
 	 
 	header("Location: $location");
 }
+
 ?>
